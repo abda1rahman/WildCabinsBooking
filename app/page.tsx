2419bf2866d567ad26df969f306a37de4b14ee9e
abda1rahman <tech.engelectrical@gmail.com>
2024-04-30 21:22:31 +0300

    types 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import getCurrentUser from "./actions/getCurrentUser";
 import getListings, { IListingsParams } from "./actions/getListings";
+import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
@@ -8,25 +9,27 @@ interface HomeProps {
   searchParams: IListingsParams;
 }
 
-const  Home = async ({searchParams}: HomeProps) => {
+const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
   if (listings.length === 0) return <EmptyState showRest />;
 
   return (
-    <Container>
-      <div className='pt-24 grid grid-col-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8'>
-        {listings.map((listing) => {
-          return (
-            <ListingCard
-              currentUser={currentUser}
-              key={listing.id}
-              data={listing}
-            />
-          );
-        })}
-      </div>
-    </Container>
+    <ClientOnly>
+      <Container>
+        <div className='pt-24 grid grid-col-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8'>
+          {listings.map((listing) => {
+            return (
+              <ListingCard
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            );
+          })}
+        </div>
+      </Container>
+    </ClientOnly>
   );
-}
+};
 export default Home;
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   eslint: {
-      ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true,
   },
   images: {
     domains: [
